0b4f7066de76919a86119519044235ff05e5f030
Frederick Roy <frederick.roy@inria.fr>
2017-01-04 16:49:17 +0100

    FIX: linking with windows ; add back the missing test in framework)

---
+++ SofaNGTest/dummyfile.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-20ll6 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* This component is open-source                                               *
+*                                                                             *
+* Authors: Matthieu Nesme                                                     *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+/*****************************************************************************
+* This file is only needed to include necessary symbols which will be exported
+* by TestMessageHandler
+* it will be useless when: SofaTestNG can use SofaCreator OR SOFA_TestPlugin_API keyword
+* is removed from TestMessageHandler
+* (and this workaround is only necessary to Windows)
+******************************************************************************/
+
+#include <SofaTest/TestMessageHandler.h>
+
